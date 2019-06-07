f6f64868c7b58866eb9e8ae2a5ddc40452dda4fe
Simon Guichandut <simonguichandut@gmail.com>
2019-06-07 14:53:29 -0400

    new method

--- relax.py
+++ relax.py
@@ -11,32 +11,74 @@ Relaxation method for a two-point boundary problem with two functions
 '''
 
 import numpy as np
-import matplotlib.pyplot as plt 
-from numpy import array,linspace,ones
+import matplotlib.pyplot as plt
+import sys
+from numpy import array, linspace, ones
 
 
-
-def Driver(X, Yinit, derivs ,BCs ,      Jacobian='analytical',derivs2=None,yinit2=None):
+def Newton(X, Yinit, derivs, BCs,      Jacobian='analytical', derivs2=None, Yinit2=None  ,epsilon=1e-10, nmax=100):
 
     ''' Arguments
     X : grid (list or numpy array)
     Yinit : list of two initial arrays [y1,y2]
-    derivs : list of two functions g1,g2  (must have input arguments (x,y1,y2) )
+    derivs : function which returns [g1,g2] - must have input arguments (x,y1(x),y2(x))
     BCs : A list of the couples [which function (1 or 2) , boundary value]. For example in top description [[2,alpha] , [1,Beta]]
-    Jacobian : analytical (requires derivs2) or approximate (requires yinit2)
-
-    global m
-    m = len(x)
+    Jacobian : analytical (requires derivs2) or approximate (requires yinit2) 
+    derivs2 : second derivative functions
+    Yinit2 : second initial solutions
     '''
-    pass
+
+    global x,m
+    x,m = X,len(X)
+
+    # Checking input
+    if Jacobian == 'analytical':
+        print('Running Newton algorithm using analytical jacobian')
+        if derivs2 is None:
+            raise Exception('Analytical jacobian method requires functions for second derivatives')
+
+    elif Jacobian == 'approximate':
+        print('Running Newton algorithm using approximate jacobian')
+        if Yinit2 is None:
+            raise Exception('Approximate jacobian requires two initial solutions')
+
+
+    ya = stack_y(Yinit[0] , Yinit[1])
+    sols = [ya]  # keep track of what happens to our solution over time
+
+    if Jacobian == 'approximate':
+        yb = stack_y(Yinit2[0] , Yinit2[1])
+        sols.append(yb)
+
+    n, go = 0, 1
+    while go:
+
+        print(n)
+        J = Jac_approx(Err, ya, yb, derivs, BCs)
+        ynew = yb - np.matmul(np.linalg.inv(J), Err(yb, derivs, BCs))
+
+        ya = yb[:]
+        yb = ynew[:]
+
+        err = np.linalg.norm(yb-ya)
+#         print(err)
+        if err < 1e-10:
+            go = 0
+            print('Converged after %d iterations\n' % n)
+
+        n += 1
+        if n == nmax:
+            print('Maximum number of iterations (%d) reached\n'%nmax)
+            go = 0
+
+                
+    return yb,sols
 
 
 
+## Shape of function vector y
 
-
-# Shape of function vector y  
-
-def stack_y(y1,y2): # gives y = [ y1_0,y2_0 , y1_1,y2_1, ... , y1_m,y2_m] (what is used in solving the matrix system)
+def stack_y(y1, y2): # gives y = [ y1_0,y2_0 , y1_1,y2_1, ... , y1_m,y2_m] (what is used in solving the matrix system)
     y = []
     for i in range(m):
         y.append(y1[i])
@@ -44,98 +86,71 @@ def stack_y(y1,y2): # gives y = [ y1_0,y2_0 , y1_1,y2_1, ... , y1_m,y2_m] (what 
     return array(y)
 
 def unstack_y(y):   # gives y1 and y2, the m-points arrays
-    y1,y2= [],[]
+    y1, y2 = [], []
     for i in range(m):
         y1.append(y[2*i])
         y2.append(y[2*i+1])
-    return np.array(y1),np.array(y2)
+    return np.array(y1), np.array(y2)
 
-def get_yk(y,k):     # gives Y_k = (y1_k,y2_k), which is the input needed for G(x,y)
-    y1,y2 = unstack_y(y)
-    return [y1[k],y2[k]]
+def get_yk(y, k):     # gives Y_k = (y1_k,y2_k), which is the input needed for G(x,y)
+    y1, y2 = unstack_y(y)
+    return (y1[k], y2[k])
+
+
 
 
 # Writing problem as F=0
 
-def G(xk,y): # RHS of dY/dx=G(x).  Expecting y to be [y1(x),y2(x)] 
-    return [y[1] , 1+2*xk]
+# def G(xk, y):  # RHS of dY/dx=G(x).  Expecting y to be [y1(x),y2(x)]
+#     return [y[1], 1+2*xk]
 
-def Err(y): # error vector. Expecting y to be the m-stacked y1,y2 
-    y1,y2 = unstack_y(y)
-    F0,Fend = y1[0]-alpha , y1[-1]-beta
-#     F = [F0,0]
-    F=[F0]
-    for k in range(1,m):
-        
-        yk,ykm1 = get_yk(y,k) , get_yk(y,k-1)
-        Gk   = G(x[k] , yk)
-        Gkm1 = G(x[k-1] , ykm1)
-        
+
+def Err(y,derivs,BCs):  # error vector. Expecting y to be the m-stacked y1,y2
+    y1, y2 = unstack_y(y)
+
+    ## Boundary conditions
+    fbc1,fbc2 =  BCs[0][0] , BCs[1][0]
+    alpha,beta = BCs[0][1] , BCs[1][1]
+    F0, Fend = y1[0]-alpha, y1[-1]-beta
+    
+    F0   = y1[0]-alpha  if fbc1==0 else y2[0]-alpha
+    Fend = y1[-1]-alpha if fbc2==0 else y2[-1]-alpha
+
+    F = [F0]  # first element is BC1
+
+    ## Grid : k=1,2...m is evaluating derivatives
+    for k in range(1, m):
+
+        yk, ykm1 = get_yk(y, k), get_yk(y, k-1)
+        Gk   = derivs(x[k],    *yk)      # star to unpack output of get_yk, which is a tuple
+        Gkm1 = derivs(x[k-1], *ykm1)
+
         for i in range(2):
-            Fk = (yk[i] - ykm1[i])/(x[k]-x[k-1]) - 0.5*(Gk[i] + Gkm1[i])
+            Fk = (yk[i] - ykm1[i])/(x[k]-x[k-1]) - 0.5*(Gk[i] + Gkm1[i])   # trapezoidal derivative
             F.append(Fk)
 
-#     F.extend([0,Fend])
-#     F.extend([Fend,0])
+    # last element is BC2
     F.append(Fend)
-        
+
     return array(F)
 
 
-# Jacobians
+## Jacobians
+def Jac_approx(func, ya, yb, *args):  # jacobian from finite difference derivatives (needs two points)
 
-def Jac_approx(func,ya,yb): # jacobian from finite difference derivatives (needs two points)
-    
-    def Jmunu(mu,nu): # returns approximate of dF_mu/dy_nu
-        num   = func([ya[i] if i!=nu else yb[i] for i in range(2*m)])[mu] - func(ya)[mu]
-        den   = yb[nu]-ya[nu]
-#         prit(num)
-#         print(array([ya[i] if i!=nu else yb[i] for i in range(2*m)]) - array(ya))
-#         print(func([ya[i] if i!=nu else yb[i] for i in range(2*m)]) - func(ya))
+    def Jmunu(mu, nu):  # returns approximate of dF_mu/dy_nu
+        num = func([ya[i] if i != nu else yb[i]
+                    for i in range(2*m)],*args)[mu] - func(ya,*args)[mu]
+        den = yb[nu]-ya[nu]
         return num/den
-    
-    J = np.zeros((2*m,2*m)) # initialize matrix
+
+    J = np.zeros((2*m, 2*m))  # initialize matrix
     for mu in range(2*m):
         for nu in range(2*m):
-            J[mu][nu] = Jmunu(mu,nu)
-            
+            J[mu][nu] = Jmunu(mu, nu)
+
     return J
 
-def Jac_exact(y): # exact jacobian (problem specificc)
+
+def Jac_exact(y):  # exact jacobian (problem specificc)
     pass
-    
-            
-
-# Iterator
-            
-def Newton(func,ya,yb,nmax=100,**kwargs):
-    
-    n,go = 1,1
-    sols = [ya]
-    while go:
-        
-        sols.append(yb)
-        print(n)
-        J = Jac_approx(func,ya,yb)
-        print(np.linalg.det(J))
-        ynew = yb - np.matmul(np.linalg.inv(J) , func(yb))
-        
-        ya = yb[:]
-        yb = ynew[:]
-   
-        err = np.linalg.norm(yb-ya)
-#         print(err)
-        if err<1e-10:
-            go=0
-            print('Converged after %d iterations'%n)
-            
-        n+=1
-        if n==nmax:
-            go=0
-                
-    return sols,n
-
-
-
-
-
